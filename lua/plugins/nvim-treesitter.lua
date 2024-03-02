c72e9595be6dc090963ddf1d63a7e4d70688c805
Ralph Juhnke <r_juhnke@gmx.de>
2024-03-02 11:26:00 +0100

    elixir in treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,11 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	config = function()
-		require('nvim-treesitter.configs').setup {}
+		require('nvim-treesitter.configs').setup {
+			ensure_installed = { "elixir", "eex" },
+			highlight = {
+				enable = { "exlixir", "eex" }
+			},
+		}
 	end
 }
